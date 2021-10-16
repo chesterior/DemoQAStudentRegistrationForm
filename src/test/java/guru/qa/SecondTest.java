92b45cf35f9a3f1dabc8d8996f5a941d49a1480c
Alex <chester1.1@mail.ru>
2021-10-16 20:44:56 +0300

    added SecondTest for lesson git

---
+++ src/test/java/guru/qa/SecondTest.java
@@ -0,0 +1,7 @@
+package guru.qa;
+
+public class SecondTest {
+    public static void main(String[] args) {
+        System.out.println("домашки выполняю с большой задержкой, мне очень жаль =(");
+    }
+}
