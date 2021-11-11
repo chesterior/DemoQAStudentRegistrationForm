8ecaf7312026e733973f871a1584f931e253427d
Alex <chester1.1@mail.ru>
2021-11-12 01:51:01 +0700

    allure lesson

--- src/test/java/guru/qa/SecondTest.java
+++ src/test/java/guru/qa/SecondTest.java
@@ -2,6 +2,6 @@ package guru.qa;
 
 public class SecondTest {
     public static void main(String[] args) {
-        System.out.println("домашки выполняю с большой задержкой, мне очень жаль =(");
+        System.out.println("?????? ??????");
     }
 }
